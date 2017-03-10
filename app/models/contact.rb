5a732961cae764d567b63b5b902b6583824a4ec9
felipezad <felipezad@gmail.com>
2017-03-09 21:47:09 -0300

    Creating models dependency

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+	belongs_to :user
+	validates :nome, presence: true, length: {minimum: 2}
+	validates :email, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	has_many :contact
+	has_many :field
 	validates :password, presence: true, length: {minimum: 8}
 	validates :email, presence: true
 end
