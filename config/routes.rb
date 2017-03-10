5d53dd30cfd2387da2b958c3235b4140490343f1
felipezad <felipezad@gmail.com>
2017-03-09 21:46:20 -0300

    Config route app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :fields
+  
   resources :contacts
-  resources :users
+  resources :users do
+	resources :fields
+  end
+  
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
